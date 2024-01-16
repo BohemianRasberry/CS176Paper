96595f0aa20f82c24e7573d549f67b605ac33a31
Saging Con Yelo <96810918+BohemianRasberry@users.noreply.github.com>
2024-01-16 16:37:44 +0800

    data cleaned

--- main.py
+++ main.py
@@ -7,28 +7,73 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import StandardScaler
 
+# DATA CLEANING
 df = pd.read_csv("Mesothelioma-data.csv")
 
 # Display the first few rows of the dataset
-# print(df.head())
-
-# Get a concise summary of the DataFrame
-# print(df.info())
-
-# Descriptive statistics for numerical columns
-# print(df.describe())
+print(df.head())
 
 # Check for missing values
 # print(df.isnull().sum())
 
+# Remove row 'keep side'
+df_semi_clean = df.drop('keep side', axis=1)
 
+df_semi_clean.rename(columns={
+    'age': 'age',
+    'gender': 'gender',
+    'city': 'city',
+    'asbestos exposure': 'asbestosExposure',
+    'diagnosis method': 'diagnosisMethod',
+    'cytology': 'cytology',
+    'duration of symptoms': 'durationOfSymptoms',
+    'dyspnoea': 'dyspnoea',
+    'ache on chest': 'chestAche',
+    'weakness': 'weakness',
+    'habit of cigarette': 'cigaretteHabit',
+    'performance status': 'performaceStatus',
+    'white blood': 'whiteBlood',
+    'cell count (WBC)': 'wbcCount',
+    'hemoglobin (HGB)': 'hbg',
+    'platelet count (PLT)': 'plt',
+    'sedimentation': 'esr',
+    'blood lactic dehydrogenise (LDH)': 'ldh',
+    'alkaline phosphatise (ALP)': 'alp',
+    'total protein': 'totalProtein',
+    'albumin': 'albumin',
+    'glucose': 'glucose',
+    'pleural lactic dehydrogenise': 'pld',
+    'pleural protein': 'pleuralProtein',
+    'pleural albumin': 'pleuralAlbumin',
+    'pleural glucose': 'pleuralGlucose',
+    'dead or not': 'mortality',
+    'pleural effusion': 'pleuralEffusion',
+    'pleural thickness on tomography': 'pleuralThickness',
+    'pleural level of acidity (pH)': 'pleuralAcidity',
+    'C-reactive protein (CRP)': 'crp',
+    'class of diagnosis': 'diagnosisClass'
+}, inplace=True)
 
-# Example: Identifying outliers using Z-score
-z_scores = np.abs(stats.zscore(df.select_dtypes(include=[np.number])))
+print(df_semi_clean.head())
 
-print(z_scores)
+# Removed duplicates
+df_semi_clean = df_semi_clean.drop_duplicates()
 
-outliers = np.where(z_scores > 3)
+# Checking on outliers
+z_scores = np.abs(stats.zscore(df_semi_clean))
+outlier_indices = np.where(z_scores > 3)  # Assuming 3 as the Z-score threshold
+unique_row_indices = np.unique(outlier_indices[0])
 
-# Depending on the context, you might want to inspect these outliers before deciding to remove them.
-print(outliers)
+print("Rows with outliers:", unique_row_indices)
+
+# Removing outliers and cleaning data set
+df_clean = df_semi_clean.drop(unique_row_indices)
+df_clean.reset_index(drop=True, inplace=True)
+
+df_clean.to_csv('Mesothelioma_clean_data.csv', index = False)
+
+print(df_clean.describe())
+print(df_clean.dtypes)
+
+# DATA TRANSFORMATION
+
